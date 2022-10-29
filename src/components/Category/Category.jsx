6ce79d2688231f6df0e07497eb7fd17b2b950946
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 22:01:29 +0500

    add

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -5,8 +5,26 @@ function Category() {
   return (
     <div className='category'>
         <div className="container">
-            
+            <div className="category-page">
+                <table className="category-table">
+                    <thead>
+                        <tr className='category-thead'>
+                            <th className='category-name'>ID</th>
+                            <th className='category-names'>Toifalar</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th className='category-names'>1</th>
+                            <th className='category-names'>Model A</th>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
+
+
+        
     </div>
   )
 }
