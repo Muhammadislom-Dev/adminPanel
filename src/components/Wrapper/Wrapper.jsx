6a0472c42d4f491a6dc4149f32b0b593adde8b3e
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 13:27:52 +0500

    add

---
+++ src/components/Wrapper/Wrapper.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import file from '../../assets/img/file.png'
+import credit from '../../assets/img/creditcard.png'
+import './Wrapper.css'
+import top from '../../assets/img/top.png'
+import tolov from '../../assets/img/banknote.png'
+
+function Wrapper() {
+  return (
+    <div className="wrapper">
+      <div className="user-title">
+        <label htmlFor="" className="user-label">
+          <img src={file} alt="" className="user-logo" />
+          <input type="text" placeholder="Izlash" className="user-input" />
+        </label>
+        <div className="user-titles">
+          <button className="user-button">
+            <img src={credit} alt="" className="user-image" /> Buyurtma berish
+          </button>
+          <button className="user-buttons">
+            <img src={tolov} alt="" className="user-image" /> To'lov
+          </button>
+          <button className="user-btn">
+            <img src={top} alt="" className="user-image" /> Yangi Topshiriq
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Wrapper;
