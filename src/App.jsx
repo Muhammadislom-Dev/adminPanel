c045609706cc29050557960693dce1a04804fcea
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 15:01:20 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Product from "./components/Product/Product";
 import UserTable from "./components/UserTable/UserTable";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Aside from "./layout/Aside/Aside";
 import Navbar from "./layout/Navbar/Navbar";
+import Analitica from "./Page/Analitica/Analitica";
 
 function App() {
   return (
@@ -12,7 +15,11 @@ function App() {
         <Aside />
         <div className="App-box">
           <Wrapper />
-          <UserTable />
+          <Routes>
+            <Route path="/" element={<UserTable />} />
+            <Route path="/analitica" element={<Analitica />} />
+            <Route path="/product" element={<Product />} />
+          </Routes>
         </div>
       </div>
     </div>
