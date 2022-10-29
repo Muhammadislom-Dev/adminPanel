4db1627bbf1a899ed090c9b1e9609ada0d5477dd
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 15:01:35 +0500

    add

---
+++ src/components/Chart/PieChart/PieChart.jsx
@@ -0,0 +1,56 @@
+import { Chart as ChartJS, Tooltip, Title, ArcElement, Legend } from "chart.js";
+import { Pie } from "react-chartjs-2";
+import "./PieChart.css";
+ChartJS.register(Tooltip, ArcElement, Legend, Title);
+
+const PieChart = () => {
+  const pieData = {
+   
+    datasets: [
+      {
+        label: "My First Dataset",
+        data: [300, 50],
+        backgroundColor: ["#3733E4", "#E929E1"],
+        hoverOffset: 4,
+      },
+    ],
+  };
+  var pieOptions = {
+    maintainAspectRatio: false,
+    scales: {},
+   
+  };
+
+  return (
+    <div>
+      <div className="pie-chart">
+        <div className="pie-top">
+          <h2 className="pie-title">All</h2>
+          <select>
+            <option value="This year">This year</option>
+            <option value="2021">2021</option>
+          </select>
+        </div>
+       
+        <div className="pie-box">
+          <Pie
+            className="pie"
+            data={pieData}
+            options={pieOptions}
+            height={200}
+          ></Pie>
+          <ul className="pie-list">
+            <li className=" pie-men">
+              <p>Men (27%)</p>
+            </li>
+            <li className=" pie-women">
+              <p>Women (73%)</p>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PieChart;
---
+++ src/components/Chart/Progress/Progress.jsx
@@ -0,0 +1,78 @@
+import "./Progress.css";
+
+const Progress = () => {
+  return (
+    <>
+      <ul className="progress">
+        <li>
+          <h2>Regions</h2>
+          <span className="progress-blok">
+            <span className="progress-precent">47%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>USA</p>
+                <p>157000</p>
+              </span>
+              <progress max="100" value="47"></progress>
+            </span>
+          </span>
+          <span className="progress-blok">
+            <span className="progress-precent">17%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>Great Britain</p>
+                <p>47000</p>
+              </span>
+              <progress max="100" value="27"></progress>
+            </span>
+          </span>{" "}
+          <span className="progress-blok">
+            <span className="progress-precent">11%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>France</p>
+                <p>37000</p>
+              </span>
+              <progress max="100" value="19"></progress>
+            </span>
+          </span>
+        </li>
+        <li>
+          <span className="progress-blok">
+            <span className="progress-precent">8%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>Italia</p>
+                <p>26000</p>
+              </span>
+              <progress max="100" value="15"></progress>
+            </span>
+          </span>
+
+          <span className="progress-blok">
+            <span className="progress-precent">6%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>Kazakhstan</p>
+                <p>23000</p>
+              </span>
+              <progress max="100" value="12"></progress>
+            </span>
+          </span>
+          <span className="progress-blok">
+            <span className="progress-precent">4%</span>
+            <span className="progress-bar">
+              <span className="progress-span">
+                <p>Others</p>
+                <p>20000</p>
+              </span>
+              <progress max="100" value="9"></progress>
+            </span>
+          </span>
+        </li>
+      </ul>
+    </>
+  );
+};
+
+export default Progress;
