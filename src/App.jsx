906b9f08eea81ba6e1c77f5fe95434518445e26b
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 13:27:25 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import './App.css';
-import Navbar from './layout/Navbar/Navbar';
+import "./App.css";
+import UserTable from "./components/UserTable/UserTable";
+import Wrapper from "./components/Wrapper/Wrapper";
+import Aside from "./layout/Aside/Aside";
+import Navbar from "./layout/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
-       <Navbar />
+      <Navbar />
+      <div className="App-page">
+        <Aside />
+        <div className="App-box">
+          <Wrapper />
+          <UserTable />
+        </div>
+      </div>
     </div>
   );
 }
