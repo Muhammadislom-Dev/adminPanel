20bfc18ebe94e0d88602a90c559852ad685c1c8c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-29 22:01:37 +0500

    add

--- src/components/Wrapper/Wrapper.jsx
+++ src/components/Wrapper/Wrapper.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from 'react';
 import file from '../../assets/img/file.png'
 import credit from '../../assets/img/creditcard.png'
 import './Wrapper.css'
@@ -6,6 +6,12 @@ import top from '../../assets/img/top.png'
 import tolov from '../../assets/img/banknote.png'
 
 function Wrapper() {
+
+  const [korzinkaModal, setKorzinkaModal] = useState(false);
+  function openKorzinkaModal() {
+    setKorzinkaModal(!korzinkaModal);
+  }
+
   return (
     <div className="wrapper">
       <div className="user-title">
@@ -14,7 +20,7 @@ function Wrapper() {
           <input type="text" placeholder="Izlash" className="user-input" />
         </label>
         <div className="user-titles">
-          <button className="user-button">
+          <button  className="user-button">
             <img src={credit} alt="" className="user-image" /> Buyurtma berish
           </button>
           <button className="user-buttons">
