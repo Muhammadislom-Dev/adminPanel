8a8b5c88b77477580fb86fcb9137e48d77d3501c
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-30 23:19:57 +0500

    add

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import "./Product.css";
-
+import ProductTable from "./ProductTable/ProductTable";
 
 export default function Product() {
 
@@ -8,36 +8,7 @@ export default function Product() {
     <div className="product">
       <div className="container">
         <div className="product-page">
-          <table className="product-table">
-            <thead>
-              <tr className="product-thead">
-                <th className="product-name">Mahsulot nomlari</th>
-                <th className="product-name">Toifalar</th>
-                <th className="product-name">Narxi</th>
-                <th className="product-name">Yuklama</th>
-                <th className="product-name">Razmeri</th>
-                <th className="product-name">Status</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr className="product-tr">
-                <th className="product-names">Lux Soft Memory</th>
-                <th className="product-names">Model C</th>
-                <th className="product-names">1 600 000 so’m</th>
-                <th className="product-names">150 kg</th>
-                <th className="product-names">200 x 134 x 40</th>
-                <th className="product-names">false</th>
-              </tr>
-              <tr className="product-tr">
-                <th className="product-names">Lux Soft Memory</th>
-                <th className="product-names">Model C</th>
-                <th className="product-names">1 600 000 so’m</th>
-                <th className="product-names">150 kg</th>
-                <th className="product-names">200 x 134 x 40</th>
-                <th className="product-names">false</th>
-              </tr>
-            </tbody>
-          </table>
+            <ProductTable />
         </div>
       </div>
     </div>
