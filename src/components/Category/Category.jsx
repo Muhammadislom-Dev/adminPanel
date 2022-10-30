c6d829c9b9cbe17ad2d1746a8f2b2a677bccfb8a
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-30 23:19:50 +0500

    add

--- src/components/Category/Category.jsx
+++ src/components/Category/Category.jsx
@@ -1,32 +1,38 @@
-import React from 'react'
-import './Category.css'
+import React from "react";
+import "./Category.css";
+import CategoryTable from "./CategoryTable/CategoryTable";
+import file from "../../assets/img/file.png";
+import credit from "../../assets/img/creditcard.png";
+import top from "../../assets/img/top.png";
+import tolov from "../../assets/img/banknote.png";
 
 function Category() {
   return (
-    <div className='category'>
-        <div className="container">
-            <div className="category-page">
-                <table className="category-table">
-                    <thead>
-                        <tr className='category-thead'>
-                            <th className='category-name'>ID</th>
-                            <th className='category-names'>Toifalar</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <th className='category-names'>1</th>
-                            <th className='category-names'>Model A</th>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
+    <div className="category">
+      <div className="container">
+        <div className="user-title">
+          <label htmlFor="" className="user-label">
+            <img src={file} alt="" className="user-logo" />
+            <input type="text" placeholder="Izlash" className="user-input" />
+          </label>
+          <div className="user-titles">
+            <button className="user-button">
+              <img src={credit} alt="" className="user-image" /> Buyurtma berish
+            </button>
+            <button className="user-buttons">
+              <img src={tolov} alt="" className="user-image" /> To'lov
+            </button>
+            <button className="user-btn">
+              <img src={top} alt="" className="user-image" /> Yangi Topshiriq
+            </button>
+          </div>
         </div>
-
-
-        
+        <div className="category-page">
+          <CategoryTable />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Category
+export default Category;
---
+++ src/components/Category/CategoryTable/CategoryTable.jsx
@@ -0,0 +1,102 @@
+import * as React from 'react';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+
+const columns = [
+  { id: 'name', label: 'Name', minWidth: 0 },
+  { id: 'code', label: 'ISO\u00a0Code', minWidth: 250 },
+];
+
+function createData(name, code, population, size) {
+  const density = population / size;
+  return { name, code, population, size, density };
+}
+
+const rows = [
+  createData('India', 'IN',),
+  createData('China', 'CN',),
+  createData('Italy', 'IT',),
+  createData('United States', 'US',),
+  createData('Canada', 'CA',),
+  createData('Australia', 'AU',),
+  createData('Germany', 'DE',),
+  createData('Ireland', 'IE',),
+  createData('Mexico', 'MX',),
+  createData('Japan', 'JP',),
+  createData('France', 'FR',),
+  createData('United Kingdom', 'GB',),
+  createData('Russia', 'RU',),
+  createData('Nigeria', 'NG',),
+  createData('Brazil', 'BR',),
+];
+
+export default function CategoryTable() {
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(+event.target.value);
+    setPage(0);
+  };
+
+  return (
+    <Paper sx={{ width: '100%', overflow: 'hidden' }}>
+      <TableContainer sx={{ maxHeight: 440 }}>
+        <Table stickyHeader aria-label="sticky table">
+          <TableHead>
+            <TableRow>
+              {columns.map((column) => (
+                <TableCell
+                  key={column.id}
+                  align={column.align}
+                  style={{ minWidth: column.minWidth }}
+                >
+                  {column.label}
+                </TableCell>
+              ))}
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows
+              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              .map((row) => {
+                return (
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                    {columns.map((column) => {
+                      const value = row[column.id];
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number'
+                            ? column.format(value)
+                            : value}
+                        </TableCell>
+                      );
+                    })}
+                  </TableRow>
+                );
+              })}
+          </TableBody>
+        </Table>
+      </TableContainer>
+      <TablePagination
+        rowsPerPageOptions={[10, 25, 100]}
+        component="div"
+        count={rows.length}
+        rowsPerPage={rowsPerPage}
+        page={page}
+        onPageChange={handleChangePage}
+        onRowsPerPageChange={handleChangeRowsPerPage}
+      />
+    </Paper>
+  );
+}
