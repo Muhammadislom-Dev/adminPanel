d87eae7b05f28a32a572435c9cd494c5e9f1cf68
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2022-10-31 14:07:40 +0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Category from "./components/Category/Category";
 import Product from "./components/Product/Product";
+import ProductAbout from "./components/ProductAbout/ProductAbout";
 import UserTable from "./components/UserTable/UserTable";
 import Wrapper from "./components/Wrapper/Wrapper";
 import Aside from "./layout/Aside/Aside";
@@ -21,6 +22,7 @@ function App() {
             <Route path="/analitica" element={<Analitica />} />
             <Route path="/product" element={<Product />} />
             <Route path="/category" element={<Category />} />
+            <Route path="/product-about" element={<ProductAbout />} />
           </Routes>
         </div>
       </div>
---
+++ src/components/ProductAbout/ProductAbout.jsx
@@ -0,0 +1,132 @@
+import React from "react";
+import './ProductAbout.css'
+import file from "../../assets/img/file.png";
+import credit from "../../assets/img/creditcard.png";
+import top from "../../assets/img/top.png";
+import tolov from "../../assets/img/banknote.png";
+import Modal from "react-modal";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal1 from "@mui/material/Modal";
+import ProductTable from "../Product/ProductTable/ProductTable";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 500,
+  height: 130,
+  bgcolor: "background.paper",
+  border: "2px solid #6f6af8",
+  boxShadow: 24,
+  borderRadius: "8px",
+  p: 4,
+};
+
+const customStyles = {
+  content: {
+    top: "50%",
+    left: "86%",
+    right: "auto",
+    bottom: "auto",
+    marginRight: "-50%",
+    height: "100%",
+    width: "30%",
+    transform: "translate(-50%, -50%)",
+  },
+};
+
+const ProductAbout = () => {
+  let subtitle;
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  //Delete modal function
+
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <div className="product-about">
+      <div className="container">
+        <div className="user-title">
+          <label htmlFor="" className="user-label">
+            <img src={file} alt="" className="user-logo" />
+            <input type="text" placeholder="Izlash" className="user-input" />
+          </label>
+          <div className="user-titles">
+            <button onClick={openModal} className="user-button">
+              <img src={credit} alt="" className="user-image" /> Qo'shish
+            </button>
+            <button onClick={openModal} className="user-buttons">
+              <img src={tolov} alt="" className="user-image" /> Tahrirlash
+            </button>
+            <button onClick={handleOpen} className="user-btn">
+              <img src={top} alt="" className="user-image" /> O'chirish
+            </button>
+          </div>
+        </div>
+
+        <ProductTable />
+      </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        style={customStyles}
+        contentLabel="Example Modal"
+      >
+        <h2 className="product-about-name">Product About</h2>
+        <form action="" className="user-form">
+          <label htmlFor="" className="user-labels">
+             Country  <input type="text" required placeholder="Country" className="user-inputs" />
+          </label>
+          <label htmlFor="" className="user-labels">
+             Weight <input type="number" required placeholder="Weight" className="user-inputs" />
+          </label>
+          <label htmlFor="" className="user-labels">
+            Height  <input type="number" required placeholder="Height" className="user-inputs" />
+          </label>
+          <label htmlFor="" className="user-labels">
+            Class  <input type="text" required placeholder="Class" className="user-inputs" />
+          </label>
+          <label htmlFor="" className="user-labels">
+            Uniform  <input type="text" required placeholder="Uniform" className="user-inputs" />
+          </label>
+          <div className="user-bottom product-bottom">
+            <button type="submit" className="user-submit">Create</button>
+          </div>
+        </form>
+      </Modal>
+
+      <Modal1
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+              Siz shu user haqidagi ma'lumotlarni o'chirmoqchimisiz?
+          </Typography>
+        </Box>
+      </Modal1>
+    </div>
+  );
+};
+
+export default ProductAbout;
